e1e3cf0aea0ab62596301c51c094ee0e73910ae0
iamLoi <LoiHuynh@protonmail.com>
2019-09-08 16:41:48 -0500

    added html tag

--- src/main/java/RandomNumberGenerator/Controller.java
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
 	@RequestMapping("/")
 	public String index() {
 		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
-		return "<body style=\"background:black\"><h1 style=" + style + ">" + new Random().nextInt(5000)
-				+ "</h1></body>";
+		return "<html><body style=\"background:black\"><h1 style=" + style + ">" + new Random().nextInt(5000)
+				+ "</h1></body></html>";
 	}
 }
