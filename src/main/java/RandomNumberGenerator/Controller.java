0e10a1d087e02596efc078a191c6b6e0389dce14
Loi Huynh <huynhxloi@gmail.com>
2019-09-08 10:10:27 -0500

    first commit

---
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -0,0 +1,18 @@
+package RandomNumberGenerator;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+	@RequestMapping("/")
+	public String index() {
+		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
+		return "<body style=\"background:black\"><h1 style=" + style + ">" + new Random().nextInt(5000)
+				+ "</h1></body>";
+	}
+}
