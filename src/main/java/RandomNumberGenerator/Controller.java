3501b7482bf50b358192aa3ec03a9240b38e20ec
iamLoi <LoiHuynh@protonmail.com>
2019-09-08 16:54:27 -0500

    Recommit2

--- src/main/java/RandomNumberGenerator/Controller.java
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
 	@RequestMapping("/")
 	public String index() {
 		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
-		return "<html><body style=\"background:black\"><h1 style=" + style + ">" + new Random().nextInt(5000)
-				+ "</h1></body></html>";
+		return "<!doctypehtml><html><head><title>RandomNumber</title></head><body style=\"background:black\"><h1 style="
+				+ style + ">" + new Random().nextInt(5000) + "</h1></body></html>";
 	}
 }
