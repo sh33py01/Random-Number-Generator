b04ec9f4d60085270319ebc11362f568ff171019
iamLoi <LoiHuynh@protonmail.com>
2019-09-19 19:20:11 -0500

    Removed styling

--- src/main/java/RandomNumberGenerator/Controller.java
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Controller {
 
 	@RequestMapping("/")
-	public String index() {
-		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
-		return "<!doctypehtml><html><head><title>RandomNumber</title></head><body style=\"background:black\"><h1 style="
-				+ style + ">" + new Random().nextInt(1000001) + "</h1></body></html>";
+	public int index() {
+		return new Random().nextInt(1000001);
 	}
 }
