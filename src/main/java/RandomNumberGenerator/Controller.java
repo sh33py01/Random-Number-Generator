97021b54eeba268005a19d2661ae69664309c22b
iamLoi <LoiHuynh@protonmail.com>
2019-09-22 10:06:03 -0500

    Updated jar

--- src/main/java/RandomNumberGenerator/Controller.java
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -11,6 +11,10 @@ public class Controller {
 
 	@RequestMapping("/")
 	public int index() {
+//		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
+//		return "<!doctypehtml><html><head><title>RandomNumber</title></head><body style=\"background:black\"><h1 style="
+//				+ style + ">" + new Random().nextInt(1000001) + "</h1></body></html>";
+
 		return new Random().nextInt(1000001);
 	}
 }
